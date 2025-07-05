044b2cc3c601eea4a6ea0afb6546ca09cb003e5c
Jakob Baggesgaard Seeberg <jbse59225@edu.ucl.dk>
2025-07-05 18:53:52 +0200

    removed cors code

--- fullstack-backend/index.js
+++ fullstack-backend/index.js
@@ -9,9 +9,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const cors = require('cors')
-
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(requestLogger)
